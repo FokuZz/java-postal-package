a4cfcf50cf09a98d81f281dfdb06bba8e50260d0
FokuZz <foxsus@yandex.ru>
2023-08-29 20:44:20 +0300

    feat: добавил нужные зависимости для деплоя на сервер

--- src/main/java/ru/skyeng/javapostalpackage/PostalPackageApplication.java
+++ src/main/java/ru/skyeng/javapostalpackage/PostalPackageApplication.java
@@ -3,10 +3,11 @@ package ru.skyeng.javapostalpackage;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @OpenAPIDefinition
-public class PostalPackageApplication {
+public class PostalPackageApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PostalPackageApplication.class, args);
